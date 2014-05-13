830e0fa43a272f360a0608a757d47f152dabe94c
Daniel Suo <danielsuo@gmail.com>
2014-05-12 23:44:09 -0400

    add user profile page

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ module.exports = function(app, passport) {
   }));
 
   app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/dash', // redirect to the secure profile section
+    successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
