2e95efa19ef4033d4ce7cdf6a2f975de09682dc2
Daniel Suo <daniel@floored.com>
2014-05-20 18:20:38 -0400

    allow group creators to remove users fixes #8

--- routes/group.js
+++ routes/group.js
@@ -38,18 +38,24 @@ router.post('/:id/add', utils.isLoggedIn, function(req, res) {
             res.redirect('/group/' + req.params.id);
           })
         } else {
-          req.flash('userAddError', 'Unable to add :( Invite your friend to sign up!');
+          req.flash('groupManageMessage', 'Unable to add :( Invite your friend to sign up!');
           res.redirect('/group/' + req.params.id);
         }
       })
     })
 });
 
-router.post('/:id/remove', utils.isLoggedIn, function(req, res) {
-  global.db.Group.find(req.params.id)
-    .success(function(group) {
-
-    })
+router.get('/:gid/remove/:uid', utils.isLoggedIn, function(req, res) {
+  isGroupCreator(req.params.gid, req.user.id, function(group, user) {
+    if (user !== null) {
+      group.removeUser(user).sucess(function() {
+        var msg = 'User ' + user.email + ' removed from group ' + group.name + '.';
+        console.log(msg);
+        req.flash('groupManageMessage', msg);
+        res.redirect('/group/' + req.params.gid);
+      });
+    }
+  });
 });
 
 router.post('/:id/poll', utils.isLoggedIn, function(req, res) {
@@ -89,12 +95,16 @@ router.get('/:id', utils.isLoggedIn, function(req, res) {
     .success(function(group) {
       if (group && group.hasUser(req.user)) {
         group.getUsers().success(function(users) {
-          res.render('group/show.html', {
-            title: group.name,
-            group: group,
-            users: users,
-            message: req.flash('userAddError')
-          });
+          isGroupCreator(req.params.id, req.user.id, function(group, user) {
+            res.render('group/show.html', {
+              title: group.name,
+              group: group,
+              users: users,
+              creator: req.user.id,
+              isCreator: user !== null,
+              message: req.flash('groupManageMessage')
+            });
+          })
         })
       } else {
         res.render('error.html');
@@ -105,5 +115,21 @@ router.get('/:id', utils.isLoggedIn, function(req, res) {
       res.render('error.html');
     })
 })
+
+function isGroupCreator(gid, uid, callback) {
+  // TODO: should really replace these two queries with one on the
+  // join table
+  global.db.Group.find(gid)
+    .success(function(group) {
+      if (group && group.creator === uid) {
+        global.db.User.find(uid)
+          .success(function(user) {
+            callback(group, user);
+          })
+      } else {
+        callback(group, null);
+      }
+    })
+};
 
 module.exports = router;
